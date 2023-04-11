4b6d01be115be00228b13edb8e31d9f8f25b1743
KaterinaKha <katerinakhaa@gmail.com>
2023-04-11 23:33:16 +0200

    task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,15 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+const listElement = document.getElementById("ingredients");
+const fragment = document.createDocumentFragment();
+
+for (const ingredient of ingredients) {
+	const liElement = document.createElement("li");
+	liElement.textContent = ingredient;
+	liElement.classList.add("item");
+	liElement.classList;
+	fragment.appendChild(liElement);
+}
+
+listElement.appendChild(fragment);
+console.log(listElement);
