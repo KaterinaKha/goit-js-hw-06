cd7b4627f11b9bd493ab519b606f4506d1e3cddd
KaterinaKha <katerinakhaa@gmail.com>
2023-04-13 15:29:05 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", onFormSubmit);
+function onFormSubmit(e) {
+	e.preventDefault();
 
+	const formData = new FormData(e.currentTarget);
+
+	const email = formData.get("email");
+	const password = formData.get("password");
+	console.log(email);
+	console.log(password);
+
+	if (!email || !password) {
+		return alert("Всі поля повинні бути заповнені! Введіть ваші дані");
+	}
+	console.log(email, password);
+	e.target.reset();
+}
