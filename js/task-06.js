f1c7466dff3a41c030fcc8194e36955068a9c347
KaterinaKha <katerinakhaa@gmail.com>
2023-04-13 13:51:25 +0200

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const inputEl = document.querySelector("#validation-input");
+const dataLength = inputEl.getAttribute("data-length");
+// console.log(dataLength);
+
+inputEl.addEventListener("blur", validateLength);
+
+function validateLength() {
+	if (inputEl.value.length < dataLength) {
+		inputEl.classList.add("invalid");
+	} else {
+		inputEl.classList.remove("invalid");
+		inputEl.classList.add("valid");
+	}
+	console.log(inputEl.value.length);
+}
