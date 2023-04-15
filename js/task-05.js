326f0eb344d9e4c5e45aef4f70148e02cc6ebf11
KaterinaKha <katerinakhaa@gmail.com>
2023-04-15 16:17:38 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -5,5 +5,11 @@ inputEl.addEventListener("input", getName);
 
 function getName(event) {
 	// console.log(event.currentTarget.value);
-	outputEl.textContent = event.currentTarget.value;
+	const inputValue = event.target.value.trim();
+	// outputEl.textContent = inputValue ? inputValue : "Anonymous";
+	if (!inputValue) {
+		outputEl.textContent = "Anonymous";
+	} else {
+		outputEl.textContent = inputValue;
+	}
 }
