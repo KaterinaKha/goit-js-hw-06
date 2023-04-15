797276b52ed8b2034eee04c5c8a8779c0d89fba6
KaterinaKha <katerinakhaa@gmail.com>
2023-04-15 19:12:08 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,13 +1,14 @@
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
+}
+
 const body = document.querySelector("body");
 const changeBtnColor = document.querySelector(".change-color");
 const colorSpan = document.querySelector(".color");
-changeBtnColor.addEventListener("click", getRandomHexColor);
 
-function getRandomHexColor() {
-	const randomColor = `#${Math.floor(Math.random() * 16777215)
-		.toString(16)
-		.padStart(6, 0)}`;
-	body.style.backgroundColor = randomColor;
-	colorSpan.textContent = randomColor;
-	console.log(colorSpan);
-}
+changeBtnColor.addEventListener("click", () => {
+	colorSpan.textContent = getRandomHexColor();
+	body.style.backgroundColor = colorSpan.textContent;
+});
